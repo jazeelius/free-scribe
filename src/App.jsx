fda83b4afc6e31267e91efa4d45e8ddda0d2f4ff
jazeelius <42842694+jazeelius@users.noreply.github.com>
2024-06-12 19:40:51 -0400

    Version 1

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import HomePage from "./components/HomePage";
+import Header from "./components/Header";
+import FileDisplay from "./components/FileDisplay";
+
+
+function App() {
+  const [file, setFile] = useState(null)
+  const [audioStream, setAudioStream] = useState(null)
+
+  const isAudioAvailable = file || audioStream
+
+  function handleAudioReset(){
+    setFile(null)
+    setAudioStream(null)
+  }
+
+  return (
+    <div className="flex flex-col max-w-[1000px] mx-auto w-full">
+      <section className="min-h-screen flex flex-col">
+        <Header></Header>
+        {isAudioAvailable ? (
+          <FileDisplay handleAudioReset={handleAudioReset} file={file} audioStream={setAudioStream} />
+        ) : ( 
+        <HomePage setFile={setFile} setAudioStream={setAudioStream}/>
+          )}
+      </section>
+      <h1 className="text-green-400">
+        hello
+      </h1>
+      <footer></footer>
+    </div>
+  )
+}
+
+export default App;
